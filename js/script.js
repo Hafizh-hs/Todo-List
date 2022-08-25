b0736ec8fab3c5af95d9cd017fd3fa7564c378e0
Hafizh-hs <hafizhhadisiswo9@gmail.com>
2022-08-25 14:31:19 +0700

    fix

--- js/script.js
+++ js/script.js
@@ -10,7 +10,6 @@ let todos = [];
             txtInput.value = "";
 
             localStorage.setItem("todos",JSON.stringify(todos));
-        
             renderTodoList();
             console.table(todos);
         }
@@ -18,11 +17,10 @@ let todos = [];
         function deleteTodo(id){
             const index = todos.findIndex(todo => todo.id === id);
             todos.splice(index,1);
-            localStorage.setItem("todos",JSON.stringify(todos));
-        
-            renderTodoList();
 
-            }
+            localStorage.setItem("todos",JSON.stringify(todos));
+            renderTodoList();
+        }
 
         function updateTodoStatus(id){
             const index = todos.findIndex(todo => todo.id === id);
@@ -30,14 +28,15 @@ let todos = [];
 
             localStorage.setItem("todos",JSON.stringify(todos));
             renderTodoList();
-            
-        }
-        function renderTodoList(){
-            const todoList = document.getElementById('todo-list');
-            todoList.innerHTML = "";
 
+
+        }
+
+        function renderTodoList(){
+            const todoList = document.getElementById("todo-list");
+            todoList.innerHTML = "";
             todos.forEach(function(todo){
-            todoList.innerHTML += `<tr>
+                todoList.innerHTML += `<tr>
                             <td width="10%">
                                 <input type="checkbox" 
                                 ${todo.status === true?"checked":""}
@@ -46,21 +45,18 @@ let todos = [];
                             <td class="${todo.status === true?"text-decoration-line-through":""}">${todo.title}</td>
                             <td width="10%"><button ${todo.status===false?"disabled":""} class="btn btn-danger" onclick="deleteTodo(${todo.id})">Delete</button></td>
                         </tr>`;
-        })
-    }
+            });
+        }
 
         document.getElementById("btnAdd")
             .addEventListener("click",function(event){
                 event.preventDefault();
-
-                localStorage.setItem("todos",JSON.stringify(todos));
-                addTodo();      
+                addTodo();
         });
 
         document.addEventListener("DOMContentLoaded",() => {
 
-        todos = JSON.parse(localStorage.getItem("todos"));
-        
+        todos = JSON.parse(localStorage.getItem("todos"));        
         localStorage.setItem("todos",JSON.stringify(todos));
 
         renderTodoList();
