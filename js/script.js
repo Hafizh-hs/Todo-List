f860f7e1b1bc540c02dcd055d0b4b466e51a0189
Hafizh-hs <hafizhhadisiswo9@gmail.com>
2022-08-25 12:43:31 +0700

    fix

--- js/script.js
+++ js/script.js
@@ -30,15 +30,14 @@
 
             localStorage.setItem("todoList",JSON.stringify(todos));
             renderTodoList();
-
+// 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
             
         }
-
         function renderTodoList(){
-            const todoList = document.getElementById("todo-list");
+        todos.forEach(function(todo){
+            const todoList = document.getElementById('todo-list');
             todoList.innerHTML = "";
-            for (const todo of todos) {
-                todoList.innerHTML += `<tr>
+            todoList.innerHTML += `tr>
                             <td width="10%">
                                 <input type="checkbox" 
                                 ${todo.status === true?"checked":""}
@@ -47,8 +46,25 @@
                             <td class="${todo.status === true?"text-decoration-line-through":""}">${todo.title}</td>
                             <td width="10%"><button ${todo.status===false?"disabled":""} class="btn btn-danger" onclick="deleteTodo(${todo.id})">Delete</button></td>
                         </tr>`;
-            }
-        }
+        })
+    }
+
+        // function renderTodoList(){
+        //     const todoList = document.getElementById("todo-list");
+        //     todoList.innerHTML = "";
+        //     for (const todo of todos) {
+                // todoList.innerHTML += `<tr>
+                //             <td width="10%">
+                //                 <input type="checkbox" 
+                //                 ${todo.status === true?"checked":""}
+                //                 class="form-check-input" 
+                //                 onclick="updateTodoStatus(${todo.id})"></td>
+                //             <td class="${todo.status === true?"text-decoration-line-through":""}">${todo.title}</td>
+                //             <td width="10%"><button ${todo.status===false?"disabled":""} class="btn btn-danger" onclick="deleteTodo(${todo.id})">Delete</button></td>
+                //         </tr>`;
+        //     }
+        //     console.log(todos);
+        // }
 
         document.getElementById("btnAdd")
             .addEventListener("click",function(event){
@@ -58,11 +74,11 @@
                 addTodo();      
         });
 
-    document.addEventListener("DOMContentLoaded",() => {
+        document.addEventListener("DOMContentLoaded",() => {
 
-    todos = JSON.parse(localStorage.getItem("todoList"));
-    
-    localStorage.setItem("todoList",JSON.stringify(todos));
+        todos = JSON.parse(localStorage.getItem("todoList"));
+        
+        localStorage.setItem("todoList",JSON.stringify(todos));
 
-    renderTodoList();
-})              
+        renderTodoList();
+    })              
